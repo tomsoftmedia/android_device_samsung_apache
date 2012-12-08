c48bfa4c3347c77f5a082a5aca46f9e33d2084f0
Mike C <mcrosson_git at nusku dot net>
2012-12-08 03:24:07 +0000

    Updated property overrides

---
+++ build_props.mk
@@ -0,0 +1,26 @@
+# Common ROM version
+ROM_VERSION_STABLE = 000
+ROM_VERSION_BETA = 000
+ROM_VERSION_ALPHA = 002
+ROM_VERSION_EXP = 001
+
+BUILD_VERSION = $(ROM_VERSION_STABLE)$(ROM_VERSION_BETA)$(ROM_VERSION_ALPHA)$(ROM_VERSION_EXP)
+
+# General
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.build.user=kemonine \
+    ro.build.host=epidemic \
+	ro.developerid=kemonine \
+	ro.modversion=$(BUILD_VERSION)
+
+# Goo.im
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.goo.developerid=kemonine \
+	ro.goo.board=apache \
+	ro.goo.rom=CyanogenMod10 \
+	ro.goo.version=$(BUILD_VERSION)
+
+# Rom Manager
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.rommanager.developerid=kemonine
+
--- full_apache.mk
+++ full_apache.mk
@@ -14,6 +14,7 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(LOCAL_PATH)/apache.mk)
+$(call inherit-product, $(LOCAL_PATH)/build_props.mk)
 
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
